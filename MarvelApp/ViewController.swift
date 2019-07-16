72e69ba5a1e36fb61533a266be6571e88bf33d74
Yana <yanak.preobrazhenskaya@yandex.ru>
2019-07-16 13:08:09 +0400

    First page

---
+++ MarvelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Яна Преображенская on 16/07/2019.
+//  Copyright © 2019 Яна Преображенская. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
